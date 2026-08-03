b589a885e4cf934942a69a0fdc9a59f77a4852cd
Jimmaphy <wschaijk@outlook.com>
2026-08-03 12:54:37 +0200

    Restart of the Jimmaphy project.

--- src/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-function enableMenu() {
-    document.querySelector('header').classList.toggle('enabled');
-}
-
-function toggleMenu() {
-    document.querySelector('header').classList.toggle('hidden');
-    document.querySelector('main').classList.toggle('hidden');
-}
